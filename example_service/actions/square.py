91da38c070e74be6772cefd1fe9d524021abf45a
Andrew Godwin <andrew@aeracode.org>
2017-03-15 17:06:32 -0700

    Use request body correctly

--- example_service/actions/square.py
+++ example_service/actions/square.py
@@ -13,7 +13,7 @@ class SquareAction(Action):
     # the "conformity" library. Simple schemas can be specified inline like this;
     # more complex ones should live in a separate schemas/ package (see other examples)
     request_schema = fields.Dictionary({
-        # Requests are always dictionaries. Here we say we want a dict with exactly
+        # Request bodies are always dictionaries. Here we say we want a dict with exactly
         # one input - a float called "number"
         'number': fields.Float(),
     })
@@ -23,8 +23,8 @@ class SquareAction(Action):
         When an action is run, the request is validated, and then passed to you
         here in the run() method, where you can perform the business logic.
         """
-        # Request is a dictionary, so it's easy to pull out fields.
-        square = request['number'] ** 2
+        # Request body is a dictionary, so it's easy to pull out fields.
+        square = request.body['number'] ** 2
         # Return the result
         return ActionResponse(body={
             "square": square,
